354e54d769b0468919049fb1d1860f8099e099f7
VooDooGrayWOOD <voodoograywood@gmail.com>
2022-08-16 23:13:46 +0700

    delete path .idea

--- server/routes/comment.routes.js
+++ server/routes/comment.routes.js
@@ -1,24 +1,53 @@
 const express = require('express')
+const auth = require('../middleware/auth.middleware')
 const Comment = require("../models/Comment");
 const router = express.Router({ mergeParams: true })
 
-router.get('/', async (req, res) => {
+router
+    .route('/')
+    .get( auth, async (req, res) => {
+        try {
+            const { orderBy, equalTo } = req.query
+            const list = await Comment.find({ [orderBy]: equalTo })
+            res.status(200).send(list)
+        } catch (e) {
+            res.status(500).json({
+                message: 'На сервере произошла ошибка. Попробуйте позже.'
+            })
+        }
+    })
+    .post(auth, async (req, res) => {
+        try {
+            const newComment = await Comment.create({
+                ...req.body,
+                userId: req.user._id
+            })
+
+            res.status(200).send(newComment)
+        } catch (e) {
+            res.status(500).json({
+                message: 'На сервере произошла ошибка. Попробуйте позже.'
+            })
+        }
+    })
+
+
+router.delete('/:commentId', auth, async (req, res) => {
     try {
-        const list = await Comment.find()
-        res.status(200).send(list)
+        const { commentId } = req.params
+        const removeComment = await Comment.findById(commentId)
+
+        if (removeComment.userId.toString() === req.user._id) {
+            await removeComment.remove()
+            return res.send(null)
+        } else {
+
+        }
     } catch (e) {
         res.status(500).json({
             message: 'На сервере произошла ошибка. Попробуйте позже.'
         })
     }
-})
-
-router.post('/', async (req, res) => {
-
-})
-
-router.delete('/:id', async (req, res) => {
-
 })
 
 
