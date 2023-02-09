6d3d8248bc6dec54035763ec22ae885cd2e235af
VooDooGrayWOOD <voodoograywood@gmail.com>
2023-02-09 10:35:31 +0700

    edit app,js

--- server/app.js
+++ server/app.js
@@ -19,9 +19,9 @@ app.use('/api', routes)
 const PORT = process.env.PORT || 9000
 
 if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.resolve(__dirname, 'client', 'build')))
+    app.use(express.static(path.resolve(__dirname, '..', 'client', 'build')))
     app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+        res.sendFile(path.resolve(__dirname, '..', 'client', 'build', 'index.html'))
     })
 }
 
